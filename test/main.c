5697b5cd0ab92c07c227d1e3c43037a38e826a90
aztl5201314 <2543036788@qq.com>
2022-01-08 17:45:46 +0800

    坚持就是胜利，加油，奥里给！

--- test/main.c
+++ test/main.c
@@ -6,12 +6,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-extern int Add(int,int);
 
 int  main()
 {
     int a = 10, b = 20,c;
-    c =Add(a, b);
     printf("%d", b);
     return 0;
 }
